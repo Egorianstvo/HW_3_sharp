84b10de9896cedf8160d7161d9b2c4122b65baeb
Ego  git config --global user.email <you@example.com>
2023-02-04 16:13:43 +0300

    Finish

--- Program.cs
+++ Program.cs
@@ -1,74 +1,72 @@
 ﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу КУбов чисел 
 от 1 до N.
 */
+#nullable disable
+System.Console.WriteLine("____Tusk 1____");
 
+System.Console.Write("Enter number: ");
 
-
-System.Console.WriteLine("Enter number: ");
-
-int Number = int.Parse(Console.ReadLine());
+int Numbers = int.Parse(Console.ReadLine());
 
 int I = 1;
 
-while(I  <= Number)
+while(I  <= Numbers)
 {
     Console.Write(Math.Pow(I, 3)+ " ");
-                                                       //сделать таблицу кубов
+                                                      
     I+=1;
 
 }
+System.Console.WriteLine(".");
 
 
 
 
 
-/*
-Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-A (3,6,8); B (2,1,-7), -> 15.84
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
 
-A (7,-5, 0); B (1,-1,9) -> 11.53
+System.Console.WriteLine("____Tusk 2____");
 
-Задача 23
-
-Console.WriteLine("Entre number X1 ");
+Console.Write("Entre number X1 ");
 double numX1 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Entre number Y1 ");
+Console.Write("Entre number Y1 ");
 double numY1 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Entre number Z1 ");
+Console.Write("Entre number Z1 ");
 double numZ1 = double.Parse(Console.ReadLine());
 
 
-
-Console.WriteLine("Entre number X2 ");
+Console.Write("Entre number X2 ");
 double numX2 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Entre number Y2 ");
+Console.Write("Entre number Y2 ");
 double numY2 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Entre number Z2 ");
+Console.Write("Entre number Z2 ");
 double numZ2 = double.Parse(Console.ReadLine());
-
 
 
 double numSQRT = Math.Sqrt(Math.Pow((numX1-numX2),2)+Math.Pow((numY1-numY2),2)+Math.Pow((numZ1-numZ2),2));
 
-Console.Write(numSQRT)
-*/
+Console.WriteLine(numSQRT);
 
 
 
-/*
-Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+
+/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 14212 -> нет
 12821 -> да
 23432 -> да
 */
 
-/*
-System.Console.WriteLine("Enter number: ");                                  
+System.Console.WriteLine("____Tusk 3____");      //К сожалению смог решить задачу только как в условие, 5-ти значное число, не нашел решение
+                                                   // что бы была для любого числа задача
+System.Console.Write("Enter number: ");                                  
 
 int Number = int.Parse(Console.ReadLine());
 
@@ -86,7 +84,7 @@ else
 {
     System.Console.WriteLine("Не является ПАЛИНДРОМОМ");
 }
-*/
+
 
 /*
 int Number = Convert.ToInt32(Console.ReadLine());
